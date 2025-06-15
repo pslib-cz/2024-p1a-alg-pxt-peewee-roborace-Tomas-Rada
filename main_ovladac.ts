fd28730ae7952a890115d6d75331e67449b84bde
tomas rada <Tomas.rada.024@pslib.cz>
2025-06-15 22:16:45 +0200

    Update main_ovladac.ts

--- main_ovladac.ts
+++ main_ovladac.ts
@@ -3,10 +3,13 @@ radio.setTransmitPower(5)
 radio.setGroup(2)
 input.onButtonPressed(Button.A, function() {
     radio.sendNumber(1)
+basic.showNumber(1)
 })
 input.onButtonPressed(Button.B, function() {
     radio.sendNumber(2)
+basic.showNumber(2)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function() {
     radio.sendNumber(3)
+basic.showNumber(3)
 })
